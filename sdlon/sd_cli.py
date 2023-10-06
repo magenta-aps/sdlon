bd3e4a7da9863a097a246f99e013f993a15efa73
Andreas Kring <andreas@magenta-aps.dk>
2023-10-06 13:50:51 +0200

    feat!: [#58007] remove test connectivity feature
    BREAKING CHANGE:
    
    The test connectivity feature is removed as it is not longer used

--- sdlon/sd_cli.py
+++ sdlon/sd_cli.py
@@ -1,13 +1,11 @@
 import click
 
-from .fix_departments import unit_fixer
 from .sd_changed_at import changed_at_cli
 from .sd_changed_at_redo import cli as changed_at_redo
 from .sd_fixup import cli as sd_fixup
 from .sd_importer import cli as sd_importer
 from .sync_job_id import sync_jobid
 from .test_mo_against_sd import cli as mo_against_sd
-from .test_sd_connectivity import check_connectivity
 
 
 @click.group()
@@ -18,7 +16,6 @@ def SDTool():
 
 SDTool.add_command(sd_importer, "sd_importer")
 SDTool.add_command(mo_against_sd, "test_mo_against_sd")
-SDTool.add_command(check_connectivity)
 SDTool.add_command(sync_jobid)
 SDTool.add_command(changed_at_cli)
 SDTool.add_command(changed_at_redo)
