c74361134c0d1bc4b37e236b519d046af4045c8d
Andreas Kring <andreas@magenta-aps.dk>
2024-01-05 16:56:18 +0100

    WIP...

--- db/models.py
+++ db/models.py
@@ -1,11 +1,13 @@
 # SPDX-FileCopyrightText: Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
-from sqlalchemy import Column
+from sqlalchemy import Column, String
 from sqlalchemy import DateTime
 from sqlalchemy import Integer
 from sqlalchemy.dialects.postgresql import TEXT
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql import func
 
 
@@ -21,3 +23,11 @@ class Payload(Base):  # type: ignore
     params = Column(TEXT)
     response = Column(TEXT)
     status_code = Column(Integer)
+
+
+class Runs(Base):  # type: ignore
+    __tablename__ = "runs"
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    from_date = Column("from_date", DateTime(timezone=True))
+    to_date = Column("to_date", DateTime(timezone=True))
+    status = Column("status", String(60))
