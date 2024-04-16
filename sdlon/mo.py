2c2a664c8e0410f741df61903185a05852e2a74c
Andreas Kring <andreas@magenta.dk>
2024-04-16 08:53:32 +0200

    feat: [#60257] script for fixing elevated engagements in Silkeborg

---
+++ sdlon/mo.py
@@ -0,0 +1,162 @@
+import base64
+from datetime import datetime
+from uuid import UUID
+
+from gql import gql
+
+from sdlon.graphql import get_mo_client
+
+
+class MO:
+    def __init__(
+        self,
+        auth_server: str,
+        client_id: str,
+        client_secret: str,
+        mo_base_url: str,
+        timeout: int = 600,
+    ):
+        self.client = get_mo_client(
+            auth_server=auth_server,
+            client_id=client_id,
+            client_secret=client_secret,
+            mo_base_url=mo_base_url,
+            gql_version=21,
+            timeout=timeout,
+        )
+
+    def get_engagements(self):
+        """
+        Get all current and future engagements
+        """
+
+        query = gql(
+            """
+            query GetEngagements(
+              $from_date: DateTime!,
+              $cursor: Cursor,
+              $limit: int!
+            ) {
+              engagements(
+                filter: {from_date: $from_date, to_date: null}
+                cursor: $cursor
+                limit: $limit
+              ) {
+                objects {
+                  validities {
+                    person {
+                      cpr_number
+                      uuid
+                    }
+                    org_unit {
+                      uuid
+                      user_key
+                      name
+                      managers {
+                        employee {
+                          uuid
+                          name
+                        }
+                      }
+                    }
+                    validity {
+                      from
+                      to
+                    }
+                    user_key
+                  }
+                  uuid
+                }
+                page_info {
+                  next_cursor
+                }
+              }
+            }
+            """
+        )
+
+        def execute(cursor: str | None, objects: list[dict]) -> str:
+            response = self.client.execute(
+                query,
+                variable_values={
+                    "limit": "100",
+                    "cursor": cursor,
+                    "from_date": datetime.now().isoformat(),
+                },
+            )
+            next_cursor = response["engagements"]["page_info"]["next_cursor"]
+            objects.extend(response["engagements"]["objects"])
+            return next_cursor
+
+        objects = []
+        next_cursor = execute(None, objects)
+        while next_cursor:
+            print(base64.b64decode(next_cursor.split(":")[1]))
+            next_cursor = execute(next_cursor, objects)
+
+        return objects
+
+    def terminate_engagement(
+        self,
+        eng_uuid: UUID,
+        to_date: datetime,
+    ) -> None:
+        mutation = gql(
+            """
+            mutation TerminateEngagement(
+              $uuid: UUID!,
+              $to: DateTime!
+            ) {
+              engagement_terminate(
+                input: {to: $to, uuid: $uuid}
+              ) {
+                uuid
+              }
+            }
+            """
+        )
+
+        self.client.execute(
+            mutation, {"uuid": str(eng_uuid), "to": to_date.date().isoformat()}
+        )
+
+    def update_engagement(
+        self,
+        eng_uuid: UUID,
+        from_date: datetime,
+        to_date: datetime | None,
+        org_unit: UUID,
+    ) -> None:
+        mutation = gql(
+            """
+            mutation UpdateEngagement(
+              $uuid: UUID!,
+              $from: DateTime!,
+              $to: DateTime,
+              $org_unit: UUID!
+            ) {
+              engagement_update(
+                input: {
+                  uuid: $uuid,
+                  validity: {
+                    from: $from,
+                    to: $to
+                  },
+                  org_unit: $org_unit
+                }
+              ) {
+                uuid
+              }
+            }
+            """
+        )
+
+        self.client.execute(
+            mutation,
+            {
+                "uuid": str(eng_uuid),
+                "from": from_date.isoformat(),
+                "to": to_date.isoformat() if to_date is not None else None,
+                "org_unit": str(org_unit),
+            },
+        )
---
+++ sdlon/scripts/fix_engagements.py
@@ -0,0 +1,152 @@
+import re
+from datetime import datetime, timedelta
+from uuid import UUID
+from zoneinfo import ZoneInfo
+
+import click
+from more_itertools import one, only
+from pydantic import ValidationError
+
+from sdlon.log import setup_logging, LogLevel, anonymize_cpr
+from sdlon.mo import MO
+from sdlon.sd import SD
+
+
+REGEX_INT = re.compile("[0-9]{5}")
+
+
+@click.command()
+@click.option(
+    "--username",
+    "username",
+    type=click.STRING,
+    envvar="SD_USER",
+    required=True,
+    help="SD username"
+)
+@click.option(
+    "--password",
+    "password",
+    type=click.STRING,
+    envvar="SD_PASSWORD",
+    required=True,
+    help="SD password"
+)
+@click.option(
+    "--institution-identifier",
+    "institution_identifier",
+    type=click.STRING,
+    envvar="SD_INSTITUTION_IDENTIFIER",
+    required=True,
+    help="SD institution identifier"
+)
+@click.option(
+    "--auth-server",
+    "auth_server",
+    type=click.STRING,
+    envvar="AUTH_SERVER",
+    default="http://localhost:8090/auth",
+    help="Keycloak auth server URL"
+)
+@click.option(
+    "--client-id",
+    "client_id",
+    type=click.STRING,
+    default="dipex",
+    envvar="CLIENT_ID",
+    help="Keycloak client id"
+)
+@click.option(
+    "--client-secret",
+    "client_secret",
+    type=click.STRING,
+    required=True,
+    envvar="CLIENT_SECRET",
+    help="Keycloak client secret for the DIPEX client"
+)
+@click.option(
+    "--mo-base-url",
+    "mo_base_url",
+    type=click.STRING,
+    default="http://localhost:5000",
+    envvar="MO_URL",
+    help="Base URL for calling MO"
+)
+def main(
+    username: str,
+    password: str,
+    institution_identifier: str,
+    auth_server: str,
+    client_id: str,
+    client_secret: str,
+    mo_base_url: str,
+):
+    setup_logging(LogLevel.DEBUG)
+
+    sd = SD(username, password, institution_identifier)
+    mo = MO(auth_server, client_id, client_secret, mo_base_url)
+
+    now = datetime.now(tz=ZoneInfo("Europe/Copenhagen"))
+
+    sd_employments = sd.get_sd_employments(datetime.now())
+    engagements = mo.get_engagements()
+
+    for count, eng in enumerate(engagements):
+        # if count % 100 == 0:
+        #     print(f"--- count: {count}")
+
+        eng_uuid = UUID(eng["uuid"])
+
+        for validity in eng["validities"]:
+            org_unit = one(validity["org_unit"])
+            person = one(validity["person"])
+
+            mo_ou_uuid: str = org_unit["uuid"]
+            cpr: str = person["cpr_number"]
+            mo_emp_uuid: str = person["uuid"]
+            user_key: str = validity["user_key"]
+            mo_from_date: datetime = datetime.fromisoformat(validity["validity"]["from"])
+            mo_to_date: str = validity["validity"]["to"]
+            # UUID of the employee who is manager
+            managers = only(org_unit["managers"])
+
+
+            # print(f"Processing {cpr}...")
+
+            if not REGEX_INT.match(user_key):
+                continue
+            if managers is not None and one(managers["employee"])["uuid"] == mo_emp_uuid:
+                continue
+
+            lookup_datetime = max(now, mo_from_date + timedelta(days=1))
+
+            try:
+                sd_emp_resp = sd.get_sd_employments(
+                    effective_date=lookup_datetime.date(),
+                    cpr=cpr,
+                    employment_identifier=user_key
+                )
+                employment = one(one(sd_emp_resp.Person).Employment)
+            except (ValidationError,  ValueError):
+                print(f"{str(mo_emp_uuid)} {anonymize_cpr(cpr)} {user_key} {mo_from_date.isoformat()} {mo_to_date} {mo_ou_uuid} Not found in SD! Terminating")
+                mo.terminate_engagement(eng_uuid, now)
+                print("Terminated")
+                continue
+
+            sd_from_date = employment.EmploymentStatus.ActivationDate
+            sd_to_date = employment.EmploymentStatus.DeactivationDate
+            sd_dep_uuid = employment.EmploymentDepartment.DepartmentUUIDIdentifier
+
+            if not mo_ou_uuid == str(sd_dep_uuid):
+                print(f"{str(mo_emp_uuid)} {anonymize_cpr(cpr)} {user_key} {mo_from_date.isoformat()} {mo_to_date} {mo_ou_uuid} {sd_from_date} {sd_to_date} {str(sd_dep_uuid)}")
+                mo.update_engagement(
+                    eng_uuid,
+                    mo_from_date,
+                    datetime.fromisoformat(mo_to_date) if mo_to_date is not None else None,
+                    sd_dep_uuid,
+                )
+                print("Updated")
+
+
+if __name__ == "__main__":
+    main()
