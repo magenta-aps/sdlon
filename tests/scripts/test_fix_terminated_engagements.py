b37bdeae0bc7c10bf3b2280e53e727f3ab0c065e
Andreas Kring <andreas@magenta.dk>
2024-08-06 08:54:38 +0200

    feat: add only_timelines_for_currently_active_emps flag to get_sd_employment_map

--- tests/scripts/test_fix_terminated_engagements.py
+++ tests/scripts/test_fix_terminated_engagements.py
@@ -199,7 +199,7 @@ def test_get_emp_status_timeline_no_current_employment() -> None:
     )
 
 
-def test_get_sd_employment_map() -> None:
+def test_get_sd_employment_map_only_currently_active_emps() -> None:
     # Arrange
     sd_employments = GetEmploymentResponse(
         Person=[
@@ -225,7 +225,11 @@ def test_get_sd_employment_map() -> None:
     )
 
     # Act
-    emp_map = get_sd_employment_map(sd_employments, sd_employments_changed)
+    emp_map = get_sd_employment_map(
+        sd_employments,
+        sd_employments_changed,
+        only_timelines_for_currently_active_emps=True,
+    )
 
     # Assert
     assert emp_map == {
@@ -254,7 +258,7 @@ def test_get_sd_employment_map() -> None:
     }
 
 
-def test_get_sd_employment_map_empty_future() -> None:
+def test_get_sd_employment_map_only_currently_active_emps_empty_future() -> None:
     # Arrange
     sd_employments = GetEmploymentResponse(
         Person=[
@@ -267,7 +271,9 @@ def test_get_sd_employment_map_empty_future() -> None:
 
     # Act
     emp_map = get_sd_employment_map(
-        sd_employments, GetEmploymentChangedResponse()  # Nothing in the future
+        sd_employments,
+        GetEmploymentChangedResponse(),  # Nothing in the future
+        only_timelines_for_currently_active_emps=True,
     )
 
     # Assert
@@ -292,4 +298,75 @@ def test_get_sd_employment_map_empty_future() -> None:
                 )
             ],
         )
+    }
+
+
+def test_get_sd_employment_map() -> None:
+    # Arrange
+    sd_employments = GetEmploymentResponse(
+        Person=[
+            Person(
+                PersonCivilRegistrationIdentifier="0101011234",
+                Employment=[CURRENT_EMPLOYMENT_STATUS],
+            )
+        ]
+    )
+    sd_employments_changed = GetEmploymentChangedResponse(
+        Person=[
+            PersonWithLists(
+                PersonCivilRegistrationIdentifier="0101011234",
+                Employment=[
+                    EmploymentWithLists(
+                        EmploymentIdentifier="12345",
+                        EmploymentStatus=FUTURE_EMPLOYMENT_STATUSES,
+                        EmploymentDepartment=FUTURE_EMPLOYMENT_DEPARTMENTS,
+                    )
+                ]
+                + [
+                    EmploymentWithLists(
+                        EmploymentIdentifier="54321",
+                        EmploymentStatus=FUTURE_EMPLOYMENT_STATUSES,
+                        EmploymentDepartment=FUTURE_EMPLOYMENT_DEPARTMENTS,
+                    )
+                ],
+            )
+        ]
+    )
+
+    # Act
+    emp_map = get_sd_employment_map(
+        sd_employments,
+        sd_employments_changed,
+        only_timelines_for_currently_active_emps=False,
+    )
+
+    # Assert
+    assert emp_map == {
+        ("0101011234", "12345"): EmploymentWithLists(
+            EmploymentIdentifier="12345",
+            EmploymentDate=date(1999, 1, 1),
+            AnniversaryDate=date(1999, 1, 1),
+            EmploymentStatus=[
+                EmploymentStatus(
+                    ActivationDate=date(2000, 1, 1),
+                    DeactivationDate=date(2000, 12, 31),
+                    EmploymentStatusCode=1,
+                ),
+            ]
+            + FUTURE_EMPLOYMENT_STATUSES,
+            EmploymentDepartment=[
+                EmploymentDepartment(
+                    ActivationDate=date(2000, 1, 1),
+                    DeactivationDate=date(2000, 12, 31),
+                    DepartmentIdentifier="ABCD",
+                    DepartmentUUIDIdentifier="6220a7b8-db38-46d6-9a36-e1f432db2726",
+                )
+            ]
+            + FUTURE_EMPLOYMENT_DEPARTMENTS,
+        ),
+        ("0101011234", "54321"): EmploymentWithLists(
+            EmploymentIdentifier="54321",
+            EmploymentStatus=FUTURE_EMPLOYMENT_STATUSES,
+            EmploymentDepartment=FUTURE_EMPLOYMENT_DEPARTMENTS,
+        ),
     }
