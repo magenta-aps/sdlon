06a5e603f90d8555f3184a90c145a95068624fda
Andreas Kring <andreas@magenta.dk>
2024-08-09 08:56:50 +0200

    feat: get_update_interval and update_eng_ou

--- sdlon/scripts/unapply_ny_logic.py
+++ sdlon/scripts/unapply_ny_logic.py
@@ -73,7 +73,43 @@ def get_mo_eng_validity_map(
     return mo_eng_map
 
 
+def get_update_interval(
+    mo_validity: Validity,
+    sd_activation_date: date,
+    sd_deactivation_date: date,
+) -> tuple[datetime, datetime | None]:
+    assert sd_activation_date <= mo_validity.from_.date()
+
+    end_date: date = min(mo_validity.to.date(), sd_deactivation_date)
+    end = datetime(end_date.year, end_date.month, end_date.day)
+    end = end if not end.date() == date.max else None
+
+    return mo_validity.from_, end
+
+
+def update_eng_ou(
+    mo: MO,
+    sd_ou: UUID,
+    mo_ou: UUID,
+    cpr_empid: tuple[str, str],
+    engagement: UUID,
+    update_from: datetime,
+    update_to: datetime,
+    dry_run: bool,
+) -> None:
+    if not sd_ou == mo_ou:
+        print(f"{cpr_empid[0]}, {cpr_empid[1]}, {str(sd_ou)}, {str(mo_ou)}")
+        if not dry_run:
+            mo.update_engagement(
+                eng_uuid=engagement,
+                from_date=update_from,
+                to_date=update_to if not update_to.date() == date.max else None,
+                org_unit=sd_ou,
+            )
+
+
 def update_engs_ou(
+    sd: SD,
     mo: MO,
     sd_map: dict[tuple[str, str], EmploymentWithLists],
     mo_map: dict[tuple[str, str], dict[Validity, dict[str, str]]],
@@ -85,6 +121,7 @@ def update_engs_ou(
     from the "NY-levels" in MO.
 
     Args:
+        sd: the SD client
         mo: the MO client
         sd_map: the SD EmploymentWithLists map (from get_sd_employment_map)
         mo_map: the MO end date map (from get_mo_eng_end_date_map)
@@ -97,34 +134,9 @@ def update_engs_ou(
             print("Could not find employment in SD")
             continue
         for validity, eng_data in validity_map.items():
-            # Assumption: the MO intervals *should* be contained in the
-            # SD intervals
-            sd_department = one(
-                [
-                    dep
-                    for dep in sd_emp.EmploymentDepartment
-                    if dep.ActivationDate <= validity.from_.date()
-                    and dep.DeactivationDate >= validity.to.date()
-                ]
-            )
-
-            ou_uuid = UUID(eng_data["ou_uuid"])
-            eng_uuid = UUID(eng_data["eng_uuid"])
-
-            if not sd_department.DepartmentUUIDIdentifier == ou_uuid:
-                print(
-                    f"{cpr_empID[0]}, {cpr_empID[1]}, "
-                    f"{str(sd_department.DepartmentUUIDIdentifier)}, {str(ou_uuid)}"
-                )
-                if not dry_run:
-                    mo.update_engagement(
-                        eng_uuid=eng_uuid,
-                        from_date=validity.from_,
-                        to_date=validity.to
-                        if not validity.to.date() == date.max
-                        else None,
-                        org_unit=sd_department.DepartmentUUIDIdentifier,
-                    )
+            if validity.from_.date() < first(sd_emp.EmploymentDepartment).ActivationDate:
+                # Get missing SD intervals
+                pass
 
 
 @click.command()
@@ -204,6 +216,7 @@ def main(
     )
 
     update_engs_ou(
+        sd=sd,
         mo=mo,
         sd_map=sd_emp_map,
         mo_map=mo_eng_validity_map,
---
+++ tests/scripts/test_unapply_ny_logic.py
@@ -0,0 +1,88 @@
+from datetime import date
+from datetime import datetime
+
+import pytest
+
+from sdlon.scripts.unapply_ny_logic import get_update_interval
+from sdlon.scripts.unapply_ny_logic import Validity
+
+
+@pytest.mark.parametrize(
+    "mo_validity, sd_activation_date, sd_deactivation_date, expected_from, expected_to",
+    [
+        # MO --------==============================>
+        # SD ------+------------------------------->
+        (
+            Validity(datetime(2000, 1, 1), datetime.max),
+            date(1999, 1, 1),
+            date.max,
+            datetime(2000, 1, 1, 0, 0, 0),
+            None,
+        ),
+        # MO --------==============================>
+        # SD --------+----------------------------->
+        (
+            Validity(datetime(2000, 1, 1), datetime.max),
+            date(2000, 1, 1),
+            date.max,
+            datetime(2000, 1, 1, 0, 0, 0),
+            None,
+        ),
+        # MO --------==============================>
+        # SD ----+------------+-------------------->
+        (
+            Validity(datetime(2000, 1, 1), datetime.max),
+            date(1999, 1, 1),
+            date(2010, 1, 1),
+            datetime(2000, 1, 1, 0, 0, 0),
+            datetime(2010, 1, 1, 0, 0, 0),
+        ),
+        # MO --------==================------------>
+        # SD ----+--------------------------------->
+        (
+            Validity(
+                datetime(2000, 1, 1),
+                datetime(
+                    2010,
+                    1,
+                    1,
+                ),
+            ),
+            date(1999, 1, 1),
+            date.max,
+            datetime(2000, 1, 1, 0, 0, 0),
+            datetime(2010, 1, 1, 0, 0, 0),
+        ),
+        # MO --------==================------------>
+        # SD ----+-----------+--------------------->
+        (
+            Validity(
+                datetime(2000, 1, 1),
+                datetime(
+                    2010,
+                    1,
+                    1,
+                ),
+            ),
+            date(1999, 1, 1),
+            date(2005, 1, 1),
+            datetime(2000, 1, 1, 0, 0, 0),
+            datetime(2005, 1, 1, 0, 0, 0),
+        ),
+    ],
+)
+def test_get_update_interval(
+    mo_validity: Validity,
+    sd_activation_date: date,
+    sd_deactivation_date: date,
+    expected_from: datetime,
+    expected_to: datetime | None,
+):
+    # Act
+    update_from, update_to = get_update_interval(
+        mo_validity, sd_activation_date, sd_deactivation_date
+    )
+
+    # Assert
+    assert expected_from == update_from
+    assert expected_to == update_to
