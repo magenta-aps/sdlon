339615ac5342b099b6b5c4173888ff8c641c0d0d
Andreas Kring <andreas@magenta.dk>
2024-12-17 13:44:47 +0100

    feat: log stdout and to a (dummy) file

--- sdlon/log.py
+++ sdlon/log.py
@@ -1,15 +1,11 @@
 import logging.config
 import re
 from enum import Enum
-from uuid import uuid4
 
 import structlog
-
+from structlog.processors import CallsiteParameter
 
 CPR_REGEX = re.compile("[0-9]{10}")
-
-# Unique log context for this application
-APP_LOG_CONTEXT = uuid4()
 
 
 class LogLevel(str, Enum):
@@ -25,42 +21,70 @@ def anonymize_cpr(cpr: str) -> str:
     return cpr[:6] + "xxxx"
 
 
-def log_filter(logger, method_name, event_dict):
-    # Only log from our application log context
-    log_context = event_dict.pop("log_context", None)
-    if log_context == APP_LOG_CONTEXT:
-        return event_dict
-    raise structlog.DropEvent
+# Taken from SDTool+
+def _dont_log_graphql_responses(logger, method_name, event_dict) -> dict:
+    """Drop logs from `BaseHTTPXTransport._decode_response` (in
+    `raclients.graph.transport`), which logs *all* GraphQL responses at DEBUG level.
+    (https://git.magenta.dk/rammearkitektur/ra-clients/-/blob/master/raclients/graph/transport.py#L117)
+    """
+    module: str | None = event_dict.get("module")
+    func_name: str | None = event_dict.get("func_name")
+    if module == "transport" and func_name in (
+        "_decode_response",
+        "_construct_payload",
+    ):
+        raise structlog.DropEvent
+    return event_dict
 
 
 def get_logger():
     """
+    DEPRECATED: will be removed in future commit
     Use this function to get a logger instead of using structlog.get_logger() directly
     """
-    return structlog.get_logger().bind(log_context=APP_LOG_CONTEXT)
+    return structlog.get_logger()
 
 
 def setup_logging(log_level: LogLevel) -> None:
-    log_level_value = logging.getLevelName(log_level.value)
-
     # Disable logging from imported modules that use Pythons stdlib logging
     logging.config.dictConfig(
         {
             "version": 1,
-            "disable_existing_loggers": True,  # This line does not really seem to work
-            "root": {"level": logging.CRITICAL + 1},
+            "disable_existing_loggers": False,
+            "handlers": {
+                "default": {
+                    "level": log_level.value,
+                    "class": "logging.StreamHandler",
+                },
+                "file": {
+                    "level": log_level.value,
+                    "class": "logging.handlers.WatchedFileHandler",
+                    # Filename will be changed in future commit
+                    "filename": "/tmp/hurra.log",
+                },
+            },
+            "loggers": {
+                "root": {
+                    "handlers": ["default", "file"],
+                    "level": log_level.value,
+                    "propagate": True,
+                },
+            },
         }
     )
 
     structlog.configure(
         processors=[
-            log_filter,
+            structlog.processors.CallsiteParameterAdder(
+                [CallsiteParameter.MODULE, CallsiteParameter.FUNC_NAME],
+            ),
+            _dont_log_graphql_responses,  # type: ignore
             structlog.contextvars.merge_contextvars,
             structlog.processors.add_log_level,
             structlog.processors.StackInfoRenderer(),
             structlog.dev.set_exc_info,
-            structlog.processors.TimeStamper(),
-            structlog.dev.ConsoleRenderer(),
+            structlog.processors.TimeStamper(fmt="iso", utc=True),
+            structlog.dev.ConsoleRenderer(colors=False),
         ],
-        wrapper_class=structlog.make_filtering_bound_logger(log_level_value),
+        logger_factory=structlog.stdlib.LoggerFactory(),
     )
