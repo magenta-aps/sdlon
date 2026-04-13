b08cdbe687eb3610d2a76e3ffebee1b5e4631bf3
Lorenzo Panieri <lp@magenta-aps.dk>
2026-04-13 13:50:35 +0200

    feat!: [#69477] remove `sdlon.sd_importer`
    BREAKING CHANGE:
    
    removed `sdlon.sd_importer` cli, removed `sd_importer` subcommand from `sdlon.sd_cli`

--- sdlon/sd_cli.py
+++ sdlon/sd_cli.py
@@ -3,7 +3,6 @@ import click
 from .sd_changed_at import changed_at_cli
 from .sd_changed_at_redo import cli as changed_at_redo
 from .sd_fixup import cli as sd_fixup
-from .sd_importer import cli as sd_importer
 from .sync_job_id import sync_jobid
 from .test_mo_against_sd import cli as mo_against_sd
 
@@ -14,7 +13,6 @@ def SDTool():
     pass
 
 
-SDTool.add_command(sd_importer, "sd_importer")
 SDTool.add_command(mo_against_sd, "test_mo_against_sd")
 SDTool.add_command(sync_jobid)
 SDTool.add_command(changed_at_cli)
