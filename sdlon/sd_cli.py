8ac765c249eaf693f095746e2c63be1e1426aa01
Lorenzo Panieri <lp@magenta-aps.dk>
2026-04-13 14:30:13 +0200

    feat!: [#69477] remove `sdlon.sd_fixup`
    BREAKING CHANGE:
    
    removed `sdlon.sd_fixup` cli, removed `sd_fixup` subcommand from `sdlon.sd_cli` cli

--- sdlon/sd_cli.py
+++ sdlon/sd_cli.py
@@ -2,7 +2,6 @@ import click
 
 from .sd_changed_at import changed_at_cli
 from .sd_changed_at_redo import cli as changed_at_redo
-from .sd_fixup import cli as sd_fixup
 from .sync_job_id import sync_jobid
 from .test_mo_against_sd import cli as mo_against_sd
 
@@ -17,8 +16,6 @@ SDTool.add_command(mo_against_sd, "test_mo_against_sd")
 SDTool.add_command(sync_jobid)
 SDTool.add_command(changed_at_cli)
 SDTool.add_command(changed_at_redo)
-SDTool.add_command(sd_fixup)
-
 
 if __name__ == "__main__":
     SDTool()
