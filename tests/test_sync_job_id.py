37bf6f867c0efa1e514674adc46cfdc780004bf8
Lorenzo Panieri <lp@magenta-aps.dk>
2026-04-15 15:47:30 +0200

    test: [#69477] unit test for `sdlon.sync_job_id.JobIdSync._edit_klasse_title`

--- tests/test_sync_job_id.py
+++ tests/test_sync_job_id.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
+from unittest import mock
+from unittest.mock import MagicMock
+from uuid import uuid4
 
 from sdlon.config import Settings
-from sdlon.graphql import get_mo_client
+from sdlon.graphql import GraphQLClient
 from sdlon.sync_job_id import JobIdSync
 
 
@@ -28,17 +31,57 @@ class Test_sync_job_id(TestCase):
             sd_global_from_date="2022-01-09",
             app_dbpassword="secret",
         )
-        mo_graphql_client = get_mo_client(
-            settings.job_settings.auth_realm,
-            settings.job_settings.client_id,
-            settings.job_settings.client_secret,  # type: ignore
-            settings.mora_base,
-            29,
-        )
         assert isinstance(settings.sd_institution_identifier, str)
         self.job_id_sync = JobIdSyncTest(
-            settings, settings.sd_institution_identifier, mo_graphql_client
+            settings, settings.sd_institution_identifier, MagicMock()
         )
 
     def test_create(self):
         self.assertTrue(self.job_id_sync.update_job_functions)
+
+    def test__edit_klasse_title(self):
+        # Arrange
+        class_uuid = str(uuid4())
+        facet_uuid = str(uuid4())
+        mock_gql_client = MagicMock(spec=GraphQLClient)
+        execute_side_effect = [
+            {
+                "classes": {
+                    "objects": [
+                        {
+                            "current": {
+                                "user_key": "user_key",
+                                "facet_response": {"uuid": facet_uuid},
+                            }
+                        }
+                    ]
+                }
+            },
+            {"class_update": {"uuid": class_uuid}},
+        ]
+        mock_execute = MagicMock(side_effect=execute_side_effect)
+        mock_gql_client.execute = mock_execute
+        self.job_id_sync.mo_graphql_client = mock_gql_client
+
+        # Act
+        self.job_id_sync._edit_klasse_title(class_uuid, "title")
+
+        # Assert
+        mock_execute.assert_has_calls(
+            [
+                mock.call(mock.ANY, variable_values={"uuid": class_uuid}),
+                mock.call(
+                    mock.ANY,
+                    variable_values={
+                        "input": {
+                            "uuid": class_uuid,
+                            "name": "title",
+                            "user_key": "user_key",
+                            "facet_uuid": facet_uuid,
+                            "validity": {"from": "1930-01-01"},
+                            "scope": "TEXT",
+                        }
+                    },
+                ),
+            ]
+        )
